1fe0732aeb1f71a25a6024e298d7fe0616c1f3b8
AleBiCi <alebianchiceriani@gmail.com>
2024-07-15 15:55:04 +0200

    Cleanup

--- CCS Project/SunScanner/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"/Users/alessandrobianchiceriani/Desktop/Ale\ Varie\ DONTTOUCH/UNITN/Terzo\ Anno/Primo\ Semestre/Embedded\ Software/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ccs/msp432p4xx_driverlib.lib"
-
